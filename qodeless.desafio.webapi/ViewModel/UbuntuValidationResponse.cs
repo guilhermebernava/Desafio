474760cc1444392e88bb2fe2f8010a7b48972fa4
dionisiohenriq <henrique.contractor@am.jll.com>
2022-01-24 17:07:40 -0300

    viewmodel de validação

---
+++ qodeless.desafio.webapi/ViewModel/UbuntuValidationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace qodeless.desafio.webapi.ViewModel
+{
+    public class UbuntuValidationResponse
+    {
+        public string Nome { get; set; }
+        public bool isValid { get; set; }
+    }
+}
